dcea720f80c405f3744f82cba182dcaf5e7bb6a3
Gregor Martynus <gregor@martynus.net>
2020-07-15 16:03:08 -0700

    feat: GHES 2.21
    BREAKING CHANGE: GHES 2.17 removed

--- src/generated/ghe-219-admin-endpoints.ts
+++ src/generated/ghe-219-admin-endpoints.ts
@@ -2,9 +2,26 @@ import { EndpointsDefaultsAndDecorations } from "../types";
 const Endpoints: EndpointsDefaultsAndDecorations = {
   enterpriseAdmin: {
     addAuthorizedSshKey: ["POST /setup/api/settings/authorized-keys"],
-    checkConfigurationStatus: ["GET /setup/api/configcheck"],
-    checkMaintenanceStatus: ["GET /setup/api/maintenance"],
-    createGlobalHook: [
+    checkConfigurationStatus: [
+      "GET /setup/api/configcheck",
+      {},
+      { renamed: ["enterpriseAdmin", "getConfigurationStatus"] },
+    ],
+    checkMaintenanceStatus: [
+      "GET /setup/api/maintenance",
+      {},
+      { renamed: ["enterpriseAdmin", "getMaintenanceStatus"] },
+    ],
+    createEnterpriseServerLicense: ["POST /setup/api/start"],
+    createGlobalHooks: [
+      "POST /admin/hooks",
+      {
+        headers: { accept: "application/vnd.github.superpro-preview+json" },
+        mediaType: { previews: ["superpro"] },
+      },
+      { renamed: ["enterpriseAdmin", "createGlobalWebhooks"] },
+    ],
+    createGlobalWebhook: [
       "POST /admin/hooks",
       {
         headers: { accept: "application/vnd.github.superpro-preview+json" },
@@ -14,11 +31,32 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     createImpersonationOAuthToken: [
       "POST /admin/users/:username/authorizations",
     ],
+    createIndexingJob: ["POST /staff/indexing_jobs"],
     createOrg: ["POST /admin/organizations"],
-    createPreReceiveEnvironment: ["POST /admin/pre-receive-environments"],
-    createPreReceiveHook: ["POST /admin/pre-receive-hooks"],
+    createPreReceiveEnvironment: [
+      "POST /admin/pre-receive-environments",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
+    createPreReceiveHook: [
+      "POST /admin/pre-receive-hooks",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
     createUser: ["POST /admin/users"],
     deleteGlobalHook: [
+      "DELETE /admin/hooks/:hook_id",
+      {
+        headers: { accept: "application/vnd.github.superpro-preview+json" },
+        mediaType: { previews: ["superpro"] },
+      },
+      { renamed: ["enterpriseAdmin", "deleteGlobalWebhook"] },
+    ],
+    deleteGlobalWebhook: [
       "DELETE /admin/hooks/:hook_id",
       {
         headers: { accept: "application/vnd.github.superpro-preview+json" },
@@ -31,17 +69,45 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
     deletePersonalAccessToken: ["DELETE /admin/tokens/:token_id"],
     deletePreReceiveEnvironment: [
       "DELETE /admin/pre-receive-environments/:pre_receive_environment_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
     deletePreReceiveHook: [
       "DELETE /admin/pre_receive_hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
     deletePublicKey: ["DELETE /admin/keys/:key_ids"],
     deleteUser: ["DELETE /admin/users/:username"],
+    demoteSiteAdministrator: ["DELETE /users/:username/site_admin"],
     demoteSiteAdministratorToOrdinaryUser: [
       "DELETE /users/:username/site_admin",
+      {},
+      { renamed: ["enterpriseAdmin", "demoteSiteAdministrator"] },
     ],
     enableOrDisableMaintenanceMode: ["POST /setup/api/maintenance"],
+    getAllAuthorizedSshKeys: ["GET /setup/api/settings/authorized-keys"],
+    getConfigurationStatus: ["GET /setup/api/configcheck"],
+    getDownloadStatusForPreReceiveEnvironment: [
+      "GET /admin/pre-receive-environments/:pre_receive_environment_id/downloads/latest",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
     getGlobalHook: [
+      "GET /admin/hooks/:hook_id",
+      {
+        headers: { accept: "application/vnd.github.superpro-preview+json" },
+        mediaType: { previews: ["superpro"] },
+      },
+      { renamed: ["enterpriseAdmin", "getGlobalWebhook"] },
+    ],
+    getGlobalWebhook: [
       "GET /admin/hooks/:hook_id",
       {
         headers: { accept: "application/vnd.github.superpro-preview+json" },
@@ -49,19 +115,49 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
       },
     ],
     getLicenseInformation: ["GET /enterprise/settings/license"],
+    getMaintenanceStatus: ["GET /setup/api/maintenance"],
     getPreReceiveEnvironment: [
       "GET /admin/pre-receive-environments/:pre_receive_environment_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
     getPreReceiveEnvironmentDownloadStatus: [
       "GET /admin/pre-receive-environments/:pre_receive_environment_id/downloads/latest",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+      {
+        renamed: [
+          "enterpriseAdmin",
+          "getDownloadStatusForPreReceiveEnvironment",
+        ],
+      },
     ],
-    getPreReceiveHook: ["GET /admin/pre-receive-hooks/:pre_receive_hook_id"],
+    getPreReceiveHook: [
+      "GET /admin/pre-receive-hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
     getPreReceiveHookForOrg: [
       "GET /orgs/:org/pre-receive-hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
     getPreReceiveHookForRepo: [
       "GET /repos/:owner/:repo/pre-receive-hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
+    getSettings: ["GET /setup/api/settings"],
     getTypeStats: ["GET /enterprise/stats/:type"],
     listGlobalHooks: [
       "GET /admin/hooks",
@@ -69,39 +165,130 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
         headers: { accept: "application/vnd.github.superpro-preview+json" },
         mediaType: { previews: ["superpro"] },
       },
+      { renamed: ["enterpriseAdmin", "listGlobalWebhooks"] },
+    ],
+    listGlobalWebhooks: [
+      "GET /admin/hooks",
+      {
+        headers: { accept: "application/vnd.github.superpro-preview+json" },
+        mediaType: { previews: ["superpro"] },
+      },
     ],
     listPersonalAccessTokens: ["GET /admin/tokens"],
-    listPreReceiveEnvironments: ["GET /admin/pre-receive-environments"],
-    listPreReceiveHooks: ["GET /admin/pre-receive-hooks"],
-    listPreReceiveHooksForOrg: ["GET /orgs/:org/pre-receive-hooks"],
-    listPreReceiveHooksForRepo: ["GET /repos/:owner/:repo/pre-receive-hooks"],
-    modifySettings: ["PUT /setup/api/settings"],
+    listPreReceiveEnvironments: [
+      "GET /admin/pre-receive-environments",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
+    listPreReceiveHooks: [
+      "GET /admin/pre-receive-hooks",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
+    listPreReceiveHooksForOrg: [
+      "GET /orgs/:org/pre-receive-hooks",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
+    listPreReceiveHooksForRepo: [
+      "GET /repos/:owner/:repo/pre-receive-hooks",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
+    listPublicKeys: ["GET /admin/keys"],
+    modifySettings: [
+      "PUT /setup/api/settings",
+      {},
+      { renamed: ["enterpriseAdmin", "setSettings"] },
+    ],
     pingGlobalHook: [
       "POST /admin/hooks/:hook_id/pings",
       {
         headers: { accept: "application/vnd.github.superpro-preview+json" },
         mediaType: { previews: ["superpro"] },
       },
+      { renamed: ["enterpriseAdmin", "pingGlobalWebhook"] },
     ],
-    promoteOrdinaryUserToSiteAdministrator: ["PUT /users/:username/site_admin"],
-    queueIndexingJob: ["POST /staff/indexing_jobs"],
+    pingGlobalWebhook: [
+      "POST /admin/hooks/:hook_id/pings",
+      {
+        headers: { accept: "application/vnd.github.superpro-preview+json" },
+        mediaType: { previews: ["superpro"] },
+      },
+    ],
+    promoteOrdinaryUserToSiteAdministrator: [
+      "PUT /users/:username/site_admin",
+      {},
+      { renamed: ["enterpriseAdmin", "promoteUserToBeSiteAdministrator"] },
+    ],
+    promoteUserToBeSiteAdministrator: ["PUT /users/:username/site_admin"],
+    queueIndexingJob: [
+      "POST /staff/indexing_jobs",
+      {},
+      { renamed: ["enterpriseAdmin", "createIndexingJob"] },
+    ],
     removeAuthorizedSshKey: ["DELETE /setup/api/settings/authorized-keys"],
-    removeEnforcementOverridesForPreReceiveHookForOrg: [
+    removePreReceiveHookEnforcementForOrg: [
       "DELETE /orgs/:org/pre-receive-hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
-    removeEnforcementOverridesForPreReceiveHookForRepo: [
+    removePreReceiveHookEnforcementForRepo: [
       "DELETE /repos/:owner/:repo/pre-receive-hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
-    renameOrg: ["PATCH /admin/organizations/:org"],
-    renameUser: ["PATCH /admin/users/:username"],
-    retrieveAuthorizedSshKeys: ["GET /setup/api/settings/authorized-keys"],
-    retrieveSettings: ["GET /setup/api/settings"],
+    renameOrg: [
+      "PATCH /admin/organizations/:org",
+      {},
+      { renamed: ["enterpriseAdmin", "updateOrgName"] },
+    ],
+    renameUser: [
+      "PATCH /admin/users/:username",
+      {},
+      { renamed: ["enterpriseAdmin", "updateUsernameForUser"] },
+    ],
+    retrieveAuthorizedSshKeys: [
+      "GET /setup/api/settings/authorized-keys",
+      {},
+      { renamed: ["enterpriseAdmin", "getAllAuthorizedSshKeys"] },
+    ],
+    retrieveSettings: [
+      "GET /setup/api/settings",
+      {},
+      { renamed: ["enterpriseAdmin", "getSettings"] },
+    ],
+    setSettings: ["PUT /setup/api/settings"],
     startConfigurationProcess: ["POST /setup/api/configure"],
+    startPreReceiveEnvironmentDownload: [
+      "POST /admin/pre-receive-environments/:pre_receive_environment_id/downloads",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+    ],
     suspendUser: ["PUT /users/:username/suspended"],
     syncLdapMappingForTeam: ["POST /admin/ldap/teams/:team_id/sync"],
     syncLdapMappingForUser: ["POST /admin/ldap/users/:username/sync"],
     triggerPreReceiveEnvironmentDownload: [
       "POST /admin/pre-receive-environments/:pre_receive_environment_id/downloads",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
+      { renamed: ["enterpriseAdmin", "startPreReceiveEnvironmentDownload"] },
     ],
     unsuspendUser: ["DELETE /users/:username/suspended"],
     updateGlobalHook: [
@@ -110,23 +297,53 @@ const Endpoints: EndpointsDefaultsAndDecorations = {
         headers: { accept: "application/vnd.github.superpro-preview+json" },
         mediaType: { previews: ["superpro"] },
       },
+      { renamed: ["enterpriseAdmin", "updateGlobalWebhook"] },
+    ],
+    updateGlobalWebhook: [
+      "PATCH /admin/hooks/:hook_id",
+      {
+        headers: { accept: "application/vnd.github.superpro-preview+json" },
+        mediaType: { previews: ["superpro"] },
+      },
     ],
     updateLdapMappingForTeam: ["PATCH /admin/ldap/teams/:team_id/mapping"],
     updateLdapMappingForUser: ["PATCH /admin/ldap/users/:username/mapping"],
+    updateOrgName: ["PATCH /admin/organizations/:org"],
     updatePreReceiveEnvironment: [
       "PATCH /admin/pre-receive-environments/:pre_receive_environment_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
     updatePreReceiveHook: [
       "PATCH /admin/pre_receive_hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
     updatePreReceiveHookEnforcementForOrg: [
       "PATCH /orgs/:org/pre-receive-hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
     updatePreReceiveHookEnforcementForRepo: [
       "PATCH /repos/:owner/:repo/pre-receive-hooks/:pre_receive_hook_id",
+      {
+        headers: { accept: "application/vnd.github.eye-scream-preview+json" },
+        mediaType: { previews: ["eye-scream"] },
+      },
     ],
+    updateUsernameForUser: ["PATCH /admin/users/:username"],
     upgradeLicense: ["POST /setup/api/upgrade"],
-    uploadLicenseForFirstTime: ["POST /setup/api/start"],
+    uploadLicenseForFirstTime: [
+      "POST /setup/api/start",
+      {},
+      { renamed: ["enterpriseAdmin", "createEnterpriseServerLicense"] },
+    ],
   },
 };
 
