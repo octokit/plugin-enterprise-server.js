f16ec9a50cc519f7c8fa60b45fa58a416bb78e00
wolfy1339 <webmaster@wolfy1339.com>
2024-04-09 12:46:11 -0400

    feat: drop GHES 3.8 support
    BREAKING CHANGE: drop GHES 3.8 support

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@ import { Octokit } from "@octokit/core";
 import { VERSION } from "./version.js";
 import { endpointsToMethods } from "./endpoints-to-methods.js";
 
-import ENDPOINTS_38 from "./generated/ghe-38-endpoints.js";
-import ADMIN_ENDPOINTS_38 from "./generated/ghe-38-admin-endpoints.js";
-
 import ENDPOINTS_39 from "./generated/ghe-39-endpoints.js";
 import ADMIN_ENDPOINTS_39 from "./generated/ghe-39-admin-endpoints.js";
 
@@ -19,16 +16,6 @@ import ADMIN_ENDPOINTS_311 from "./generated/ghe-311-admin-endpoints.js";
 
 import ENDPOINTS_312 from "./generated/ghe-312-endpoints.js";
 import ADMIN_ENDPOINTS_312 from "./generated/ghe-312-admin-endpoints.js";
-
-export function enterpriseServer38Admin(octokit: Octokit) {
-  return endpointsToMethods(octokit, ADMIN_ENDPOINTS_38);
-}
-enterpriseServer38Admin.VERSION = VERSION;
-
-export function enterpriseServer38(octokit: Octokit) {
-  return endpointsToMethods(octokit, ENDPOINTS_38);
-}
-enterpriseServer38.VERSION = VERSION;
 
 export function enterpriseServer39Admin(octokit: Octokit) {
   return endpointsToMethods(octokit, ADMIN_ENDPOINTS_39);
--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -1,19 +1,19 @@
-import { enterpriseServer38, enterpriseServer38Admin } from "../src/index.ts";
+import { enterpriseServer312, enterpriseServer312Admin } from "../src/index.ts";
 
 describe("Smoke test", () => {
-  it("enterpriseServer35 is a function", () => {
-    expect(enterpriseServer38).toBeInstanceOf(Function);
+  it("enterpriseServer312 is a function", () => {
+    expect(enterpriseServer312).toBeInstanceOf(Function);
   });
 
-  it("enterpriseServer35Admin is a function", () => {
-    expect(enterpriseServer38Admin).toBeInstanceOf(Function);
+  it("enterpriseServer312Admin is a function", () => {
+    expect(enterpriseServer312Admin).toBeInstanceOf(Function);
   });
 
-  it("enterpriseServer35.VERSION is set", () => {
-    expect(enterpriseServer38.VERSION).toEqual("0.0.0-development");
+  it("enterpriseServer312.VERSION is set", () => {
+    expect(enterpriseServer312.VERSION).toEqual("0.0.0-development");
   });
 
-  it("enterpriseServer35Admin.VERSION is set", () => {
-    expect(enterpriseServer38Admin.VERSION).toEqual("0.0.0-development");
+  it("enterpriseServer312Admin.VERSION is set", () => {
+    expect(enterpriseServer312Admin.VERSION).toEqual("0.0.0-development");
   });
 });
