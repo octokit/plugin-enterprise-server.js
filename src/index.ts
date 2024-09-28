a5f581b93e7eaf5fe0e52a94f5285b05cd87d90f
wolfy1339 <webmaster@wolfy1339.com>
2024-09-27 23:25:21 -0400

    chore: remove unsupported GHES verions
    BREAKING CHANGE: remove GHES 3.9 and GHES 3.10 support

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,11 @@ import { Octokit } from "@octokit/core";
 import { VERSION } from "./version.js";
 import { endpointsToMethods } from "./endpoints-to-methods.js";
 
-import ENDPOINTS_39 from "./generated/ghe-39-endpoints.js";
-import ADMIN_ENDPOINTS_39 from "./generated/ghe-39-admin-endpoints.js";
-
-import ENDPOINTS_310 from "./generated/ghe-310-endpoints.js";
-import ADMIN_ENDPOINTS_310 from "./generated/ghe-310-admin-endpoints.js";
-
 import ENDPOINTS_311 from "./generated/ghe-311-endpoints.js";
 import ADMIN_ENDPOINTS_311 from "./generated/ghe-311-admin-endpoints.js";
 
 import ENDPOINTS_312 from "./generated/ghe-312-endpoints.js";
 import ADMIN_ENDPOINTS_312 from "./generated/ghe-312-admin-endpoints.js";
-
-export function enterpriseServer39Admin(octokit: Octokit) {
-  return endpointsToMethods(octokit, ADMIN_ENDPOINTS_39);
-}
-enterpriseServer39Admin.VERSION = VERSION;
-
-export function enterpriseServer39(octokit: Octokit) {
-  return endpointsToMethods(octokit, ENDPOINTS_39);
-}
-enterpriseServer39.VERSION = VERSION;
-
-export function enterpriseServer310Admin(octokit: Octokit) {
-  return endpointsToMethods(octokit, ADMIN_ENDPOINTS_310);
-}
-enterpriseServer310Admin.VERSION = VERSION;
-
-export function enterpriseServer310(octokit: Octokit) {
-  return endpointsToMethods(octokit, ENDPOINTS_310);
-}
-enterpriseServer310.VERSION = VERSION;
 
 export function enterpriseServer311Admin(octokit: Octokit) {
   return endpointsToMethods(octokit, ADMIN_ENDPOINTS_311);
